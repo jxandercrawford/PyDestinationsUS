32b06ba04118701e7fd6f03425cdb90dc0547720
xc383 <xc383@drexel.edu>
2023-06-22 09:44:30 -0400

    Add docstrings to pipeline.

--- src/pipeline.py
+++ src/pipeline.py
@@ -12,14 +12,28 @@ class Pipeline:
         self.__records = None
 
     def can_write(self):
+        """
+        Returns if the pipeline state allows for writing.
+        :returns: Boolean if can write to database.
+        """
         if self.__records:
             return True
         return False
 
     def extract_records(self, year: int, month: int) -> None:
+        """
+        Extract flight records from the Bereau of Transporation Statistics.
+        :param year (int): The target year to extract.
+        :param month (int): The target month to extract.
+        :returns: None.
+        """
         self.__records = self.__extracter.extract_flights(year, month)
 
     def write_records(self) -> None:
+        """
+        Write flight records to the database.
+        :returns: None.
+        """
         if not self.__records:
             raise RuntimeError("The pipeline is not in the correct state. Please run `extract_records()` before writing.")
 
@@ -35,6 +49,15 @@ class Pipeline:
         self.__records = None
 
     def run(self, year: int, month: int) -> int:
+        """
+        Run the full pipeline. This consists of:
+        1. Extracting date for year and month.
+        2. Writing data to database.
+
+        :param year (int): The target year to extract.
+        :param month (int): The target month to extract.
+        :returns: The integer of number of records extracted and written.
+        """
         self.extract_records(year, month)
         n_inserted = len(self.__records["flight"])
         self.write_records()
